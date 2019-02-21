11b686f736e81a0e47f801ae75d4b7040b88909f
Steven An (MacBook Air) <stevenan@gmail.com>
2019-02-20 20:03:06 -0500

    update

---
+++ spaceninjas/tilemaps/powerup.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="powerup" tilewidth="16" tileheight="16" tilecount="4" columns="2">
+ <image source="../sprites/Spaceship-shooter-environment/spritesheets/power-up.png" trans="faffea" width="32" height="32"/>
+ <tile id="0" type="turret"/>
+</tileset>
